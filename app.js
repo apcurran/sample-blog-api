4d1fc6438be3afa915146197cbbea296a60fbc8a
Alex Curran <alexcurran.va@gmail.com>
2020-08-21 13:56:38 -0500

    Added CORS anywhere

---
+++ app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const express = require("express");
+const feedRoutes = require("./routes/feed");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    next();
+});
+
+app.use("/feed", feedRoutes);
+
+app.listen(5000);
---
+++ routes/feed.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require("express");
+const feedController = require("../controllers/feed");
+
+const router = express.Router();
+
+router.get("/posts", feedController.getPosts);
+
+router.post("/post", feedController.createPost);
+
+module.exports = router;
