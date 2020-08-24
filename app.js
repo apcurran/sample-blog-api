ed2c40e4c1a123151d5f94fd5ef2c47402a51b05
Alex Curran <alexcurran.va@gmail.com>
2020-08-24 15:57:23 -0500

    Added error handling

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 "use strict";
 
+require("dotenv").config();
+
 const express = require("express");
 const feedRoutes = require("./routes/feed");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use("/images", express.static("images"));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,5 +21,19 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use((error, req, res, next) => {
+    console.log(error);
 
-app.listen(5000);
+    const status = error.statusCode || 500;
+    const message = error.message;
+
+    res.status(status).json({
+        message: message
+    });
+});
+
+mongoose
+    .connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => app.listen(5000))
+    .catch(err => console.error(err));
+
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,17 +1,58 @@
 "use strict";
 
+const { validationResult } = require("express-validator");
+const Post = require("../models/Post");
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{ title: "First Post", content: "This is the first post." }]
+        posts: [
+            {
+                _id: 1,
+                title: "First Post",
+                content: "This is the first post.",
+                imageUrl: "images/matcha-tea.jpg",
+                creator: {
+                    name: "Alex"
+                },
+                createdAt: new Date()
+            }
+        ]
     });
 }
 
-exports.createPost = (req, res, next) => {
-    const { title, content } = req.body;
+exports.createPost = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+    
+        if (!errors.isEmpty()) {
+            const error = new Error("Validation failed, incorrect data");
+            error.statusCode = 422;
+    
+            throw error;
+        }
+    
+        const { title, content } = req.body;
+        const post = new Post({
+            title: title,
+            content: content,
+            imageUrl: "images/matcha-tea.jpg",
+            creator: {
+                name: "Alex"
+            }
+        });
+        const savedPost = await post.save();
+        console.log(savedPost);
+    
+        res.status(201).json({
+            message: "Post created successfully.",
+            post: savedPost
+        });
+        
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
 
-    // Create post in db
-    res.status(201).json({
-        message: "Post created successfully.",
-        post: { id: new Date().toString(), title: title, content: content }
-    });
+        next(err);
+    }
 }
