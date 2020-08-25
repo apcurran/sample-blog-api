8f1a37f2540d42bab3d6708a8a6358dcb2f09df4
Alex Curran <alexcurran.va@gmail.com>
2020-08-25 10:48:32 -0500

    Post editing functional

--- app.js
+++ app.js
@@ -5,11 +5,32 @@ require("dotenv").config();
 const express = require("express");
 const feedRoutes = require("./routes/feed");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + "-" + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
 // Middleware
 app.use(express.json());
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use("/images", express.static("images"));
 
 app.use((req, res, next) => {
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,23 +1,27 @@
 "use strict";
 
+const fs = require("fs");
+const path = require("path");
+
 const { validationResult } = require("express-validator");
 const Post = require("../models/Post");
 
-exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: 1,
-                title: "First Post",
-                content: "This is the first post.",
-                imageUrl: "images/matcha-tea.jpg",
-                creator: {
-                    name: "Alex"
-                },
-                createdAt: new Date()
-            }
-        ]
-    });
+exports.getPosts = async (req, res, next) => {
+    try {
+        const posts = await Post.find();
+
+        res.status(200).json({
+            message: "Fetched all posts successfully.",
+            posts
+        });
+
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+
+        next(err);
+    }
 }
 
 exports.createPost = async (req, res, next) => {
@@ -30,12 +34,20 @@ exports.createPost = async (req, res, next) => {
     
             throw error;
         }
+
+        if (!req.file) {
+            const error = new Error("No image provided.");
+            error.statusCode = 422;
+
+            throw error;
+        }
     
+        const imageUrl = req.file.path;
         const { title, content } = req.body;
         const post = new Post({
             title: title,
             content: content,
-            imageUrl: "images/matcha-tea.jpg",
+            imageUrl: imageUrl,
             creator: {
                 name: "Alex"
             }
@@ -55,4 +67,97 @@ exports.createPost = async (req, res, next) => {
 
         next(err);
     }
+}
+
+exports.getPost = async (req, res, next) => {
+    const { postId } = req.params;
+
+    try {
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            const error = new Error("Could not find post.");
+            error.statusCode = 404;
+
+            throw error;
+        }
+
+        res.status(200).json({
+            message: "Post fetched.",
+            post: post
+        });
+        
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+
+        next(err);
+    }
+}
+
+exports.updatePost = async (req, res, next) => {
+    try {
+        // Validate data first
+        const errors = validationResult(req);
+        
+        if (!errors.isEmpty()) {
+            const error = new Error("Validation failed, incorrect data");
+            error.statusCode = 422;
+            
+            throw error;
+        }
+
+        const { postId } = req.params;
+        const { title, content } = req.body;
+        let imageUrl = req.body.image;
+
+        if (req.file) {
+            imageUrl = req.file.path;
+        }
+    
+        if (!imageUrl) {
+            const error = new Error("No file picked.");
+            error.statusCode = 422;
+    
+            throw error;
+        }
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            const error = new Error("Could not find post.");
+            error.statusCode = 404;
+
+            throw error;
+        }
+
+        if (imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+
+        post.title = title;
+        post.content = content;
+        post.imageUrl = imageUrl;
+
+        const updatedPost = await post.save();
+
+        res.status(200).json({
+            message: "Post updated",
+            post: updatedPost
+        });
+
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+
+        next(err);
+    }
+}
+
+// Helper function
+function clearImage(filePath) {
+    filePath = path.join(__dirname, "..", filePath);
+    fs.unlink(filePath, err => console.error(err));
 }
