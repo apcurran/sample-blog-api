fb0a1f5a1ab968f5dccd5073ba4d7cc4251163fc
Alex Curran <alexcurran.va@gmail.com>
2020-08-27 15:16:33 -0500

    Included unit tests for auth-middleware using chai and mocha

---
+++ test/auth-middleware.js
@@ -0,0 +1,54 @@
+const { expect } = require("chai");
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
+
+const authMiddleware = require("../middleware/is-auth");
+
+describe("Auth middleware", function() {
+    it("Should throw an error if no authorization header is present", function() {
+        const req = {
+            get() {
+                return null;
+            }
+        };
+    
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw("Not authenticated."); // dummy empty object for res and empty func for next
+    });
+    
+    it("Should throw an error if the auth header is only one string without a space", function() {
+        const req = {
+            get() {
+                return "xyz";
+            }
+        };
+    
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    });
+
+    it("Should throw an error if the token cannot be verified", function() {
+        const req = {
+            get() {
+                return "Bearer xyz";
+            }
+        };
+
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    });
+
+    it("Should yield a userId after decoding the token", function() {
+        const req = {
+            get() {
+                return "Bearer xlajsdkfd";
+            }
+        };
+
+        // Mock verify function
+        sinon.stub(jwt, "verify");
+        jwt.verify.returns({ userId: "abc" });
+
+        authMiddleware(req, {}, () => {});
+        expect(req).to.have.property("userId");
+        // Restore original jwt verify function
+        jwt.verify.restore();
+    });
+});
---
+++ test/start.js
@@ -0,0 +1,17 @@
+// const expect = require("chai").expect;
+
+// Practice test
+// it("Should add numbers correctly", () => {
+//     const num1 = 2;
+//     const num2 = 4;
+
+//     expect(num1 + num2).to.equal(6);
+// });
+
+// it("Should not give a result of 7", () => {
+//     const num1 = 2;
+//     const num2 = 4;
+
+//     expect(num1 + num2).not.to.equal(7);
+// });
+
